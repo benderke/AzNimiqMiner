480b13ea8089818fcf20ae1ae70724d402604e6e
Sander Verhoeven <Sander.Verhoeven@recomatics.com>
2020-06-13 22:47:20 +0200

    upgrade to 0.6.1 (improved hashrate)

--- nim.sh
+++ nim.sh
@@ -4,11 +4,11 @@ sudo apt-get -y upgrade
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth unzip
 
 #latest beta-miner
-wget https://github.com/Beeppool/miner/releases/download/0.6.0/beepminer-0.6.0.zip
-unzip beepminer-0.6.0.zip
-cd beepminer-0.6.0
+wget https://github.com/Beeppool/miner/releases/download/0.6.1/beepminer-0.6.1.zip
+unzip beepminer-0.6.1.zip
+cd beepminer-0.6.1
 mkdir donation
-cp -r beepminer-0.6.0/* donation
+cp -r beepminer-0.6.1/* donation
 
 pool_address1="${pool_address1:-us.nimpool.io:8444}"
 #multiply donation by 10 because we're running for 1000 minutes, not 100 minutes
@@ -16,7 +16,7 @@ let donation*=10
 
 for i in `seq 1 4`;
 do
-    cd beepminer-0.6.0
+    cd beepminer-0.6.1
     sudo timeout 1000m ./miner --wallet-address="$wallet1" --pool=$pool_address1 --deviceLabel=$miner_id
     cd ..
     if [ $donation -gt 0 ]
